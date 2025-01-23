5d282320d1897292117d6c5c345ac69df422c315
Hacault14 <nathanhacault@gmail.com>
2025-01-22 21:20:17 -0500

    change colour

--- config.ts
+++ config.ts
@@ -83,8 +83,8 @@ const config = {
     theme: "light",
     // REQUIRED — This color will be reflected on the whole app outside of the document (loading bar, Chrome tabs, etc..). By default it takes the primary color from your DaisyUI theme (make sure to update your the theme name after "data-theme=")
     // OR you can just do this to use a custom color: main: "#f37055". HEX only.
-    main: "#20c95e",    // Primary color
-    accent: "#20c95e",  // Accent color
+    main: "#32CD32",    // Primary color
+    accent: "#32CD32",  // Accent color
   },
   auth: {
     // REQUIRED — the path to log in users. It's use to protect private routes (like /dashboard). It's used in apiClient (/libs/api.js) upon 401 errors from our API
